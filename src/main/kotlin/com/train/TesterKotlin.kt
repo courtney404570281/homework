2976884bc2d455e34d8c8b6b29e4d4f5cdd3f46e
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-03-03 13:56:14 +0800

    Calculate the price of tickets

--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -1,5 +1,24 @@
 package com.train
 
-fun main(args: Array<String>) {
+import java.lang.Math.round
+import java.util.*
 
+fun main(args: Array<String>) {
+    val scanner = Scanner(System.`in`)
+
+    print("Please enter number of tickets: ")
+    var allTickets = scanner.nextInt()
+
+    print("How many round-trip tickets: ")
+    val roundTrip = scanner.nextInt()
+
+    val ticket = Tickets(allTickets - roundTrip, roundTrip)
+    print("Total tickets: " + allTickets + "\n" + "Round-trip: " + roundTrip + "\n" + "Total: " + ticket.total())
+}
+
+class Tickets(var oneWay: Int, var roundTrip: Int){
+    fun total(): Int {
+        val total = 1000 * oneWay + round((2000 * roundTrip) * 0.9f)
+        return total
+    }
 }
