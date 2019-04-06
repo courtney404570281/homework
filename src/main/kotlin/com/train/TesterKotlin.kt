83068bee3448fa4b7bb3e97527f50bdd48bc1f1a
courtney404570281 <404570281@gapp.fju.edu.tw>
2019-04-06 21:56:40 +0800

    Add conditions to end the services in Kotlin

--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -8,20 +8,33 @@ fun main() {
     val scanner = Scanner(System.`in`)
 
     while (leave != -1) {
+
         // 輸入欲購買之票數
         print("Please enter number of tickets: ")
         var allTickets = scanner.nextInt()
+
         // 輸入來回票之票數
         print("How many round-trip tickets: ")
         val roundTrip = scanner.nextInt()
 
-        val ticket = Tickets(allTickets - roundTrip, roundTrip)
-        print("Total tickets: " + allTickets + "\n" + "Round-trip: " + roundTrip + "\n" + "Total: " + ticket.total())
+        if (allTickets < 1 || roundTrip < 0) {
 
-        // 是否結束本程式
-        println("Do you want to end the service?(if yes, please enter -1): ")
-        leave = scanner.nextInt()
+            println("Please enter the TRUE number!")
+
+        } else {
+
+            // 計算票價與張數
+            val ticket = Tickets(allTickets - roundTrip, roundTrip)
+            print("Total tickets: " + allTickets + "\n" + "Round-trip: " + roundTrip + "\n" + "Total: " + ticket.total())
+
+            // 是否繼續交易
+            println("Next Transaction?(if yes, please enter 0. Otherwise, -1)")
+            leave = scanner.nextInt()
+        }
+
     }
+
+    println("Thanks for having me!")
 }
 
 class Tickets(var oneWay: Int, var roundTrip: Int){
